2a1b636d9fc2d89c8fceeca46a191d069216026f
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-02-08 12:42:23 -0600

    :sparkles: Update the access to firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,4 @@
-import firebase from "@react-native-firebase/app";
-
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyD6p0Lxq6znAL0CZ5fVrHF_0ReEY6KtbYo",
   authDomain: "congalcodeprueba.firebaseapp.com",
   databaseURL: "https://congalcodeprueba-default-rtdb.firebaseio.com",
@@ -9,15 +7,3 @@ const firebaseConfig = {
   messagingSenderId: "520969330295",
   appId: "1:520969330295:web:2a4432ebaeb46cd4753072"
 };
-
-const initializeFirebaseApp = () => {
-  if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-    return true;
-  } else {
-    console.log('Hay un error mi pana');
-    return false;
-  }
-}
-
-export default initializeFirebaseApp;
