0ae324cfe8bd74577431621daf3fe3953799df07
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-02-26 12:53:00 -0600

    :lipstick: Improve code style

--- src/routes/Room.jsx
+++ src/routes/Room.jsx
@@ -1,11 +1,20 @@
 import { View, StyleSheet, Text } from "react-native";
+import SmartHomeLogo from "../components/SmartHomeLogo";
 import globalTheme from "../theme/global-theme";
+import useRoom from '../hooks/useRoom.js';
 
 export default function Room({ route }) {
   const { id } = route.params;
+  const { room } = useRoom(id);
 
   return <View style={styles.container}>
+    <SmartHomeLogo
+      title={room.name}
+      subtitle={`Administra tu ${ room.name }`}
+    />
+
     <Text>Hello World {id}</Text>
+    <Text>Hello World {room.name}</Text>
   </View>;
 }
 
