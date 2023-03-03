dc4a9a011e832ef6297f083230d8eae7db15660f
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-03-03 00:10:29 -0600

    :art: Add real time modification for sensor list

--- src/Main.jsx
+++ src/Main.jsx
@@ -6,45 +6,23 @@ import { useEffect } from "react";
 import { useRecoilState } from "recoil";
 import { db } from "./firebase/firebase.js";
 import { sensorDataListState } from "./states/sensors.js";
-import { sensorsDataState } from "./states/data-sensors.js";
 import SafeAppBar from "./components/SafeAppBar";
 import Home from "./routes/Home";
 import Room from "./routes/Room";
 import Sensor from "./routes/Sensor";
 import Register from "./routes/Register";
 import Login from "./routes/Login";
-import { sensorsData } from "./utils/sensors-data.js";
 
 const Stack = createStackNavigator();
 
 const Main = () => {
-  const [ sensorDataList, setSensorDataList ] = useRecoilState(sensorDataListState);
-  const [ sensorData, setSensorData ] = useRecoilState(sensorsDataState);
-
-  const { sensors } = sensorsData();
+  const [ _, setSensorDataList ] = useRecoilState(sensorDataListState);
 
   useEffect(() => {
     const startRoute = ref(db, '/');
     onValue(startRoute, snapshot => {
       const info = snapshot.val();
       setSensorDataList(info);
-
-      let newSensors = [];
-
-      sensors.map(s => {
-        let sensorAux = { ...s };
-
-        if (s.concrete) {
-          sensorAux.lectures[0] = info[s.firebaseName] === 0
-            ? 'NO' : 'SI';
-        } else {
-          sensorAux.lectures[0] = `${ info[s.firebaseName] }`;
-        }
-
-        newSensors.push({ ...sensorAux });
-      });
-
-      console.log(newSensors);
     });
   }, []);
 
--- src/states/data-sensors.js
+++
@@ -1,6 +0,0 @@
-import { atom } from "recoil";
-
-export const sensorsDataState = atom({
-  key: 'sensorsDataState',
-  default: null
-});
